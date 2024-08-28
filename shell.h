d3b70e76029631292b0af30f69c6daddabac5433
devcharis <joshuabamidele9@gmail.com>
2024-08-28 09:44:47 +0100

    lets try tis

--- shell.h
+++ shell.h
@@ -9,5 +9,15 @@
 
 extern char **environ;
 
+/* Function prototypes */
+void display_prompt(void);
+char *read_input(void);
+int execute_command(char *command);
+char *_getenv(const char *name);
+int _setenv(const char *name, const char *value, int overwrite);
+int _unsetenv(const char *name);
+void free_memory(char **args);
+void handle_sigint(int sig);
+
 #endif /* SHELL_H */
 
